5e0d3c356823077b313660549112e5f7d179f144
Tavi <secret9179@gmail.com>
2022-08-22 22:26:05 +0900

    git test

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -4,7 +4,7 @@ import "../components/TodoTemplate.scss";
 const TodoTemplate = ({ children }) => {
   return (
     <div className="TodoTemplate">
-      <div className="app-title">일정 관리 하기</div>
+      <div className="app-title">일정 관리 안함</div>
       <div className="content">{children}</div>
     </div>
   );
